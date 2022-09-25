b9103d8ce765f891df1e3632d78c0b0f6a028f73
ButukDamir <trxsh69@mail.ru>
2022-09-25 22:31:34 +0500

    форма оранж и длинная

--- second/second/Form1.Designer.cs
+++ second/second/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(103, 78);
+            this.button1.Location = new System.Drawing.Point(321, 115);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(282, 103);
+            this.button1.Size = new System.Drawing.Size(69, 29);
             this.button1.TabIndex = 0;
             this.button1.Text = "закрыть";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,8 +45,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.ClientSize = new System.Drawing.Size(485, 268);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(720, 268);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "блокнот";
